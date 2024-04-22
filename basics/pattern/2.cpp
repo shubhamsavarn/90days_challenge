a4b10db6c37fcb64560da748e9ca044bc5570093
Shubham Savarn <54256934+shubhamsavarn@users.noreply.github.com>
2024-04-22 14:11:26 +0530

    Create 2.cpp
    4321
    4321
    4321
    4321

---
+++ basics/pattern/2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cin >> n >> m;
+    for(int i =0;i<n;i++){
+       for(int j =0;j<m;j++){
+           cout << m-j;
+       }
+       cout << endl;
+    }
+    return 0;
+}
